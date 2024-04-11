7b28b17248e56b255293a17a035e2f984ba95b8c
johan-STYL <johan@styldesign.se>
2024-04-11 21:50:17 +0200

    example pocketbase app

--- main.go
+++ main.go
@@ -1,64 +1,24 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"net/http"
+	"log"
 	"os"
-	"time"
 
-	"github.com/charmbracelet/log"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
 )
 
-const VERSION = "0.0.1"
-
 func main() {
-	startTime := time.Now()
+    app := pocketbase.New()
 
-	flagVersion := flag.Bool("v", false, "version")
-	flagDebug := flag.Bool("debug", false, "Printing all log levels")
-	flagDev := flag.Bool("dev", false, "loggs include caller and debug level")
-	flag.Parse()
+    // serves static files from the provided public dir (if exists)
+    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+        e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+        return nil
+    })
 
-	if *flagVersion {
-		fmt.Println("Version: " + VERSION)
-		os.Exit(0)
-	}
-
-
-	logger := log.New(os.Stderr)
-	logger.SetPrefix("main")
-	logger.SetReportTimestamp(true)
-
-	if *flagDebug {
-		logger.SetLevel(log.DebugLevel)
-	}
-	if *flagDev {
-		logger.SetReportCaller(true)
-	}
-
-	l := logger.WithPrefix(logger.GetPrefix() + ".http")
-	router := newRouter(l)
-	router.prepareRoutes()
-
-	logger.Info(
-		"handing over to http server",
-		logTimeSpent, time.Since(startTime),
-	)
-
-	logger.Fatal(l, runServer(l, &router))
-}
-
-func runServer(l *log.Logger, s *server) error {
-
-	pageSrv := http.Server{
-		Addr:    ":8080",
-		Handler: s.Handler(),
-	}
-
-	l.Info("Starting server", "addr", pageSrv.Addr)
-	if err := pageSrv.ListenAndServe(); err != nil {
-		return fmt.Errorf("ListenAndServe: %s", err)
-	}
-	return fmt.Errorf("unexpected server shutdown")
+    if err := app.Start(); err != nil {
+        log.Fatal(err)
+    }
 }
