78da12798f8c5a851070e72eff26155355466e3e
Johan Strand <jst@jst.dev>
2024-08-16 18:40:26 +0200

    initial template code

--- main.go
+++ main.go
@@ -1,64 +1,88 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
+	"io"
+	"log"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
 	"time"
-
-	"github.com/charmbracelet/log"
 )
 
-const VERSION = "0.0.1"
-
-func main() {
-	startTime := time.Now()
-
-	flagVersion := flag.Bool("v", false, "version")
-	flagDebug := flag.Bool("debug", false, "Printing all log levels")
-	flagDev := flag.Bool("dev", false, "loggs include caller and debug level")
-	flag.Parse()
-
-	if *flagVersion {
-		fmt.Println("Version: " + VERSION)
-		os.Exit(0)
-	}
-
-
-	logger := log.New(os.Stderr)
-	logger.SetPrefix("main")
-	logger.SetReportTimestamp(true)
-
-	if *flagDebug {
-		logger.SetLevel(log.DebugLevel)
-	}
-	if *flagDev {
-		logger.SetReportCaller(true)
-	}
-
-	l := logger.WithPrefix(logger.GetPrefix() + ".http")
-	router := newRouter(l)
-	router.prepareRoutes()
-
-	logger.Info(
-		"handing over to http server",
-		logTimeSpent, time.Since(startTime),
-	)
-
-	logger.Fatal(l, runServer(l, &router))
+type Config struct {
+	Host string
+	Port string
 }
 
-func runServer(l *log.Logger, s *server) error {
+func main() {
 
-	pageSrv := http.Server{
-		Addr:    ":8080",
-		Handler: s.Handler(),
+	ctx := context.Background()
+
+	if err := run(ctx,
+		// os.Args,
+		// os.Getenv,
+		// os.Stdin,
+		// os.Stdout,
+		os.Stderr,
+	); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(
+	ctx context.Context,
+	// args []string,
+	// getenv func(string) string,
+	// stdin io.Reader,
+	// stdout io.Writer,
+	stderr io.Writer,
+) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	config := Config{
+		Host: "",
+		Port: "8080",
 	}
 
-	l.Info("Starting server", "addr", pageSrv.Addr)
-	if err := pageSrv.ListenAndServe(); err != nil {
-		return fmt.Errorf("ListenAndServe: %s", err)
+	logger := log.New(stderr, "", log.LstdFlags)
+	server := NewServer(ctx, logger)
+	httpServer := &http.Server{
+		Addr:    net.JoinHostPort(config.Host, config.Port),
+		Handler: server,
 	}
-	return fmt.Errorf("unexpected server shutdown")
+	go func() {
+		log.Printf("listening on %s\n", httpServer.Addr)
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Fprintf(os.Stderr, "error listening and serving: %s\n", err)
+		}
+	}()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+		log.Println("shutting down http server")
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := httpServer.Shutdown(shutdownCtx); err != nil {
+			fmt.Fprintf(os.Stderr, "error shutting down http server: %s\n", err)
+		}
+	}()
+	wg.Wait()
+	return nil
+}
+
+func NewServer(
+	ctx context.Context,
+	logger *log.Logger,
+) http.Handler {
+	mux := http.NewServeMux()
+	addRoutes(mux, logger)
+	return mux
 }
